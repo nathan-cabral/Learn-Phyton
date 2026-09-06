43341830f055514338088f91c415468fc90aeaed
nathancabrall <nathaneliascabral@gmail.com>
2026-09-06 16:44:38 -0300

    py

--- POO/classes.py
+++ POO/classes.py
@@ -1,6 +1,15 @@
 class Canal:
     def __init__(self,nome,descricao,inscritos):
         self.nome=nome
+        self.descricao=descricao
+        self.inscritos=inscritos
+    def inscrever(self,quantidade=1):
+        self.inscritos+=quantidade
 
 canal_nathan=Canal("nathan","opa salve",12)
-print(canal_nathan.nome)
+
+canal_jose=Canal("josezin GAYMER","ai pai para",10000000)
+
+print(f"inscritos atual do nathan: {canal_nathan.inscritos}")
+canal_nathan.inscrever()
+print(f"inscritos atual do nathan: {canal_nathan.inscritos}")
